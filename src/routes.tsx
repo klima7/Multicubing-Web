6ef90f64c740c6ee8311d25105ab96953383b3b6
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-05 00:20:00 +0100

    Router added

---
+++ src/routes.tsx
@@ -0,0 +1,10 @@
+import { Routes, Route } from 'react-router';
+import App from './App';
+
+const routes = (
+  <Routes>
+    <Route path="/abc" element={<App />}/>
+  </Routes>
+);
+
+export default routes;
