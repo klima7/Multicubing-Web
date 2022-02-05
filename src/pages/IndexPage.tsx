579b5d203e9537250843e95e9b555991bda39b13
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-05 02:17:46 +0100

    Font awesome icons added

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,10 +1,13 @@
 import DeleteIcon from '@mui/icons-material/Delete';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 
 function IndexPage() {
   return (
     <div>
         <h1>Index Page</h1>
         <DeleteIcon />
+        <FontAwesomeIcon icon={faCoffee} />
     </div>
   );
 }
