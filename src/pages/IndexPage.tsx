dd62544e66f1a3b730dcec901c1ded0b41ab4ce1
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-05 10:22:22 +0100

    Favicon added

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -2,11 +2,13 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCoffee } from '@fortawesome/free-solid-svg-icons'
 import { Coffee } from 'mdi-material-ui'
+import Button from '@mui/material/Button';
 
 function IndexPage() {
   return (
     <div>
         <h1>Index Page</h1>
+        <Button variant="contained">Hello Material UI!</Button>
         <DeleteIcon />
         <FontAwesomeIcon icon={faCoffee} />
         <Coffee />
