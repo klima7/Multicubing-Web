0b93ba9a0d537ed6227dceea352e737022cbf230
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-06 23:24:11 +0100

    Simple login page

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -1,7 +1,27 @@
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 function LoginPage() {
   return (
     <div>
-        <h1>Login Page</h1>
+        <Grid container>
+          <Grid item xs={12} style={{textAlign: 'center'}}>
+            <Box sx={{mt: 10}}>
+              <Paper variant="outlined" style={{display: 'inline-block', width: '60ex', borderColor: '#0000ff', borderWidth: '1.2pt', padding: '10pt'}}>
+                <h1>Login</h1>
+                <div><TextField label="Login" variant="outlined" style={{width: '100%', marginBottom: '10px'}}/></div>
+                <div><TextField label="Password" variant="outlined" type="password" style={{width: '100%'}} /></div>
+                <div style={{textAlign: 'left'}}><FormControlLabel control={<Checkbox defaultChecked />} label="Remember me" /></div>
+                <div><Button variant="contained" style={{width: '100%'}}>Login</Button></div>
+              </Paper>
+            </Box>
+          </Grid>
+        </Grid>
     </div>
   );
 }
