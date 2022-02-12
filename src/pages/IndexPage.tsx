fa805b4f82bf42e95687e53cf21787b9c5840052
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-12 19:01:17 +0100

    Animation added to index page

--- src/pages/IndexPage.tsx
+++ src/pages/IndexPage.tsx
@@ -1,13 +1,14 @@
 import logo from '../images/logo.svg';
 import Grid from '@mui/material/Grid';
 import Box from '@mui/material/Box';
+import '../styles/index.css'
 
 function IndexPage() {
   return (
 <Grid container spacing={2} alignItems="center">
   <Grid item xs={12} md={6} style={{textAlign: "center"}}>
     <Box sx={{mt: 10}}>
-      <img src={logo} className="App-logo" alt="logo" width='300pt' />
+      <img src={logo} className="App-logo" alt="logo" width='300pt' style={{animation: 'logo-animation 3s infinite'}} />
       <h1>Multicubing</h1>
     </Box>
   </Grid>
