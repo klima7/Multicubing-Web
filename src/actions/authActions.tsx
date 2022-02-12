7039fe6a880fa1b09f167860af956d33ee8913e9
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-12 19:46:45 +0100

    Simple redux reducer added

---
+++ src/actions/authActions.tsx
@@ -0,0 +1,3 @@
+import { authSlice } from "../reducers/authReducer"
+
+export const { increment, decrement, incrementByAmount } = authSlice.actions
--- src/store.ts
+++ src/store.ts
@@ -1,7 +1,10 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
+import authReducer from './reducers/authReducer';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    authReducer: authReducer,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
