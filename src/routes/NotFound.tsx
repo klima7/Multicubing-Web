2e7ee202e7464507712c7af5f6c399ba3a8f5f7f
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-20 14:19:47 +0100

    404 page appearance improved

--- src/routes/NotFound.tsx
+++ src/routes/NotFound.tsx
@@ -1,7 +1,36 @@
-function NotFoundPage() {
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Lottie from "react-lottie";
+import HomeIcon from "@mui/icons-material/Home";
+import { Link } from "react-router-dom";
+import notFoundLottie from "../assets/lotties/not-found.json";
+import "../assets/styles/Index.css";
+
+function IndexPage() {
+  const defaultOptions = {
+    loop: false,
+    animationData: notFoundLottie,
+  };
+
   return (
-    <h1>Page not found</h1>
+    <Grid container spacing={2} sx={{ my: 7 }} alignItems="center">
+      <Grid item xs={12} md={6} style={{ textAlign: "center" }}>
+        <Lottie options={defaultOptions} width={400} />
+      </Grid>
+      <Grid item xs={12} md={6} style={{ textAlign: "center" }}>
+        <h1>Page not found</h1>
+        <Button
+          variant="contained"
+          startIcon={<HomeIcon />}
+          component={Link}
+          to={"/"}
+        >
+          Go to Home Page
+        </Button>
+      </Grid>
+    </Grid>
   );
 }
 
-export default NotFoundPage;
+export default IndexPage;
