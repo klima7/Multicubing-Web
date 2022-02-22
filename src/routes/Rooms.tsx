73b9eb053f4db2c320688becf9605926f3596947
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-22 01:56:24 +0100

    Fab button to add room

--- src/routes/Rooms.tsx
+++ src/routes/Rooms.tsx
@@ -1,6 +1,28 @@
+import React from 'react';
+import Fab from '@mui/material/Fab';
+import AddIcon from '@mui/icons-material/Add';
+import { deepOrange } from '@mui/material/colors';
+
+const fabStyle = {
+  backgroundColor: deepOrange[500], 
+  color: 'white',
+  margin: 0,
+  top: 'auto',
+  right: 30,
+  bottom: 30,
+  left: 'auto',
+  position: 'fixed',
+} as React.CSSProperties;;
+
 function RoomsPage() {
   return (
-    <h1>Rooms</h1>
+    <div>
+      <h1>Rooms</h1>
+      <Fab variant="extended" style={fabStyle}>
+        <AddIcon sx={{ mr: 1 }} />
+        Add room
+      </Fab>
+    </div>
   );
 }
 
