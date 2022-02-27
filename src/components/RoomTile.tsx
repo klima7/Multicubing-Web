7769d2680c85bfebed221870d852a981800fb989
klima7 <ukasz.klimkiewicz2@wp.pl>
2022-02-27 02:05:57 +0100

    Join button added to rooms

--- src/components/RoomTile.tsx
+++ src/components/RoomTile.tsx
@@ -1,4 +1,5 @@
 import { useHistory } from 'react-router-dom';
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
@@ -6,6 +7,7 @@ import Badge from '@mui/material/Badge';
 import Tooltip from '@mui/material/Tooltip';
 import InfoIcon from '@mui/icons-material/Info';
 import LockIcon from '@mui/icons-material/Lock';
+import { Typography } from '@mui/material';
 import { FC } from 'react';
 import { Room } from '../types/types'
 import { cubeVisualizations } from '../utils/cube-visualization';
@@ -16,13 +18,20 @@ interface Props {
 
 const RoomTile: FC<Props> = ({room}) => {
   const history = useHistory();
+  const [mouseOver, setMouseOver] = useState(false);
 
   function handleClick() {
     history.push(`/room/${room.slug}`)
   }
 
   return (
-    <Paper elevation={3} style={{textAlign: 'left', cursor: 'pointer'}} onClick={handleClick}>
+    <Paper 
+    elevation={3} 
+    style={{textAlign: 'left', cursor: 'pointer'}} 
+    onClick={handleClick}
+    onMouseEnter={() => setMouseOver(true)}
+    onMouseLeave={() => setMouseOver(false)}
+    >
       <Grid container spacing={1}>
         <Grid item xs={4}>
           <Box sx={{ml: 2, my: 1}}>
@@ -38,18 +47,29 @@ const RoomTile: FC<Props> = ({room}) => {
           </Box>
         </Grid>
         <Grid item xs={8}>
-          <Box sx={{pl: 0, mr: 1, height: '100%'}} style={{position: 'relative'}}>
+          <Box 
+          sx={{pl: 0, mr: 1, height: '100%'}} 
+          style={{position: 'relative'}}>
             <span style={{fontSize: '12pt'}}>{room.name}</span>
-            <Box style={{position: 'absolute', bottom: 0}}>
+            <Box 
+            sx={{display: 'flex'}}
+            style={{position: 'absolute', bottom: 6, width: '100%'}}
+            >
               {room.private && (
                 <Tooltip title="Private room">
-                  <LockIcon color="success" />
+                  <LockIcon color="primary" />
                 </Tooltip>
               )}
               {room.description != null && (
                 <Tooltip title={'Description: ' + room.description}>
-                  <InfoIcon />
+                  <InfoIcon color="primary" />
                 </Tooltip>
+              )}
+              <div style={{flex: 1}} />
+              {mouseOver && (
+                <Typography style={{fontWeight: 'bold'}} color="secondary">
+                Join!
+                </Typography>
               )}
             </Box>
           </Box>
