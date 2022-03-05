32bdf9c152aade4409779d8143e6df98b77b549a
klima7 <ukasz.klimkiewicz@gmail.com>
2022-03-05 23:03:21 +0100

    Lottie added to room password screen

--- src/components/RoomPasswordScreen.tsx
+++ src/components/RoomPasswordScreen.tsx
@@ -1,12 +1,22 @@
 import Box from '@mui/material/Box';
 import { Grid } from '@mui/material';
+import Lottie from "react-lottie";
 import RoomPasswordCard from "./RoomPasswordCard";
+import lockLottie from "../assets/lotties/lock.json";
 
 function RoomPasswordScreen() {
+  const lottieOptions = {
+    loop: false,
+    animationData: lockLottie,
+  };
+
   return (
     <Grid container>
       <Grid item xs={12} style={{textAlign: 'center'}}>
-        <Box sx={{mt: 10}}>
+        <Box>
+          <Box sx={{my: 3}}>
+            <Lottie options={lottieOptions} width={150} />
+          </Box>
           <RoomPasswordCard />
         </Box>
       </Grid>
